4511a0e1a47f37417bdbb5a6fbdb83fc17d2a610
LianqingChan <lianqing_chan@126.com>
2026-04-03 00:56:54 +0800

    Changes to be committed: 	modified:   web/vitepress/.vitepress/config.js 	new file:   web/vitepress/docs/.vitepress/config.js 	new file:   web/vitepress/docs/.vitepress/public/subskin_logo.png 	new file:   web/vitepress/docs/.vitepress/theme/custom.css 	new file:   web/vitepress/docs/.vitepress/theme/index.js 	new file:   web/vitepress/docs/.vitepress/theme/vars.css 	modified:   web/vitepress/node_modules/.package-lock.json 	modified:   web/vitepress/node_modules/@algolia/abtesting/README.md 	modified:   web/vitepress/node_modules/@algolia/abtesting/package.json 	modified:   web/vitepress/node_modules/@algolia/client-abtesting/README.md 	modified:   web/vitepress/node_modules/@algolia/client-abtesting/package.json 	modified:   web/vitepress/node_modules/@algolia/client-analytics/README.md 	modified:   web/vitepress/node_modules/@algolia/client-analytics/package.json 	modified:   web/vitepress/node_modules/@algolia/client-common/package.json 	modified:   web/vitepress/node_modules/@algolia/client-insights/README.md 	modified:   web/vitepress/node_modules/@algolia/client-insights/package.json 	modified:   web/vitepress/node_modules/@algolia/client-personalization/README.md 	modified:   web/vitepress/node_modules/@algolia/client-personalization/package.json 	modified:   web/vitepress/node_modules/@algolia/client-query-suggestions/README.md 	modified:   web/vitepress/node_modules/@algolia/client-query-suggestions/package.json 	modified:   web/vitepress/node_modules/@algolia/client-search/README.md 	modified:   web/vitepress/node_modules/@algolia/client-search/package.json 	modified:   web/vitepress/node_modules/@algolia/ingestion/README.md 	modified:   web/vitepress/node_modules/@algolia/ingestion/package.json 	modified:   web/vitepress/node_modules/@algolia/monitoring/README.md 	modified:   web/vitepress/node_modules/@algolia/monitoring/package.json 	modified:   web/vitepress/node_modules/@algolia/recommend/README.md 	modified:   web/vitepress/node_modules/@algolia/recommend/package.json 	modified:   web/vitepress/node_modules/@algolia/requester-browser-xhr/package.json 	modified:   web/vitepress/node_modules/@algolia/requester-fetch/package.json 	modified:   web/vitepress/node_modules/@algolia/requester-node-http/package.json 	modified:   web/vitepress/node_modules/algoliasearch/README.md 	modified:   web/vitepress/node_modules/algoliasearch/package.json 	modified:   web/vitepress/package-lock.json

---
+++ web/vitepress/docs/.vitepress/theme/index.js
@@ -0,0 +1,12 @@
+/**
+ * SubSkin Custom VitePress Theme
+ *
+ * Extends the default VitePress theme with our custom medical-tech design system.
+ */
+
+import DefaultTheme from 'vitepress/theme'
+import './custom.css'
+
+export default {
+  ...DefaultTheme,
+}
